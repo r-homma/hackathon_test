63127ea0296e54a88ee39f919e4d62e22f701154
homma <hommariku@hommanoMacBook-Pro.local>
2021-08-21 13:33:14 +0900

    2回目

---
+++ hacathon_test/blogpost/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+class SampleModel(models.Model):
+    title = models.CharField(max_length=100)
+    number = models.IntegerField()
+
+CATEGORY = (('business','ビジネス'),('life','生活'),('other','その他'))#('左','右')左：pythonの実装で表示される項目,右：htmlファイル上で表示される項目
+class BlogModel(models.Model):#以下で定義される変数は、全てオブジェクト
+    title = models.CharField(max_length=100)
+    question = models.TextField()
+    answer = models.TextField(null=True)
+    postdate = models.DateField(auto_now_add=True)
+    category = models.CharField(max_length = 50, choices = CATEGORY)
+    def __str__(self):#特殊メソッド。オブジェクトの文字列表現を返す。
+        return self.title
---
+++ hacathon_test/blogpost/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, DeleteView, UpdateView#class,djangoの標準ビュー,modelを単純にHTMLに表示させることができる
+from .models import BlogModel#model
+from django.urls import reverse_lazy
+
+# Create your views here.
+class BlogList(ListView):#BlogListがListViewclassを継承している
+    template_name = 'list.html'
+    model = BlogModel
+
+class BlogDetail(DetailView):
+    template_name = 'detail.html'
+    model = BlogModel
+
+class BlogCreate(CreateView):
+    template_name = 'create.html'
+    model = BlogModel#ユーザーが入力した情報をmodelテーブルに保存しているイメージ
+    fields = ('title', 'question', 'answer', 'category')#create.htmlで表示させる項目を記載している
+    success_url = reverse_lazy('list')
+
+class BlogDelete(DeleteView):
+    template_name = 'delete.html'
+    model = BlogModel
+    success_url = reverse_lazy('list')
+
+class BlogUpdate(UpdateView):
+    template_name = 'update.html'
+    model = BlogModel
+    fields = ('title', 'question', 'answer', 'category')
+    success_url = reverse_lazy('list')
